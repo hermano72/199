ddf8fa6996cade84f22c11a269f829da34ee953a
hermano72 <paata72@yahoo.com>
2018-12-21 13:44:26 +0400

    Update is_phone_number.py

--- ch_7/is_phone_number.py
+++ ch_7/is_phone_number.py
@@ -1,8 +1,23 @@
+
+# 555-822-5959
+
 def is_phone_number(text):
   if len(text) != 12:
     return False
   
-  if not text[:3].isalpha():
+  if not text[:3].isdecimal():
+    return False
+  
+  if text[3] != '-':
+    return False
+  
+  if not text[4:7].isdecimal():
+    return False
+  
+  if text[7] != '-':
+    return False
+  
+  if not text[8:].isdecimal():
     return False
   
   return True
@@ -12,9 +27,13 @@ def is_phone_number(text):
 # -------------------------
 message = "My phone Number is 555-228-5358\n\
 office number is 874-568-9636\n\
-My second phon number is 412-588-96-01"
+My second phon number is 412-588-9601"
 
-print(is_phone_number(message))
+for i in range(len(message)):
+  chunk = message[i:i+12]
+  
+  if is_phone_number(chunk):
+    print("Found phone number:", chunk)
 
 
 
